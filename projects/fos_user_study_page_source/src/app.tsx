4b3aa5971e2bee1dbf7f24adeba61bc72f4b7332
ziyannchen <1041276865@qq.com>
2024-05-03 11:23:45 +0800

    clear fos user study page

--- projects/fos_user_study_page_source/src/app.tsx
+++
@@ -1,6 +0,0 @@
-import { PageLoading } from '@ant-design/pro-layout';
-
-/** 获取用户信息比较慢的时候会展示一个 loading */
-export const initialStateConfig = {
-  loading: <PageLoading style={{ marginTop: '30vh' }} />,
-};
--- projects/fos_user_study_page_source/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Fos from '@/pages/home';
-
-interface IProps {}
-
-const Index: React.FC<IProps> = () => <Fos />;
-
-export default Index;
